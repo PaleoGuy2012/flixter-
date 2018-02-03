59f47a3060a486d72e8d9340be5897abaaedbbd7
Ryan Marks <Ryan@paleo2go@gmail.com>
2018-02-03 00:59:48 +0000

    Sections Drag and Drop

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :lessons, only: [:update]
+    resources :sections, only: [:update]
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
     end
@@ -15,3 +16,4 @@ Rails.application.routes.draw do
     end
   end
 end
+  
